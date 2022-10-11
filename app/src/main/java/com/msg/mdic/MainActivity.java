44b143a557416cfcf6920445c692f44cd7e4c196
miniblueplay <774418855299663z@gmail.com>
2022-10-12 02:19:44 +0800

    GIF

--- app/src/main/java/com/msg/mdic/MainActivity.java
+++ app/src/main/java/com/msg/mdic/MainActivity.java
@@ -3,12 +3,39 @@ package com.msg.mdic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import com.msg.mdic.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hideNav();
+        //使用ViewBinding後的方法
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        //mBinding.text.setText("ok");
+    }
+
+
+
+    /**隱藏狀態列和導航欄*/
+    public void hideNav() {
+        Window window = getWindow();
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
